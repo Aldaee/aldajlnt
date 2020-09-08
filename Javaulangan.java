85604bb9e102daa6cca837aad55b61d891459354
Aldaee <70958629+Aldaee@users.noreply.github.com>
2020-09-08 21:14:46 +0700

    Add files via upload

---
+++ Javaulangan.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaulangan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Javaulangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+        
+        System.out.println(alfabet);
+        System.out.println(""+alfabet.substring(0,1));
+        System.out.println(""+alfabet.substring(11,12));
+        System.out.println(""+alfabet.substring(3,4));
+        System.out.println(""+alfabet.substring(0,1));
+        System.out.println(""+alfabet.substring(9,10));
+        System.out.println(""+alfabet.substring(20,21));
+        System.out.println(""+alfabet.substring(11,12));
+        System.out.println(""+alfabet.substring(8,9));
+        System.out.println(""+alfabet.substring(0,1));
+        System.out.println(""+alfabet.substring(13,14));
+        System.out.println(""+alfabet.substring(19,20));
+        System.out.println(""+alfabet.substring(8,9));
+    }
+    
+}
